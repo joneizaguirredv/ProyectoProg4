888a5cf5ddcd38a7d36fec3b8fb2ac7776f8c6ac
Asitulom <asieriturriotz@opendeusto.es>
2022-04-09 12:29:48 +0200

    Update BBDD.c

--- TrabajoProg4/BBDD.c
+++ TrabajoProg4/BBDD.c
@@ -8,6 +8,7 @@ void crearTablas(sqlite3 *db){
 	sqlite3_stmt *stmt;
 
 	char sql[] = "create table persona(id int, nom varchar2(20))";
+	/*PRUEBA ASIER GITHUB*/
 
 
 	/*int result = */sqlite3_prepare_v2(db, sql, -1, &stmt, NULL) ;
